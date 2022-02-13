ca2938c235286392fe5838bece3a0062a1aff93f
Maskalenkas <maskalenkasdoc@gmail.com>
2022-02-12 23:06:43 -0300

    fix

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,9 +4,28 @@ import GlobalStyles from 'styles/global'
 
 import theme from 'styles/theme'
 
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+const customViewports = {
+  MobileMin: {
+    name: 'Mobile min',
+    styles: {
+      width: '280px',
+      height: '653px'
+    }
+  }
+}
+
 export const parameters = {
   nextRouter: {
     Provider: RouterContext.Provider
+  },
+  layout: 'fullscreen',
+  viewport: {
+    viewports: {
+      ...MINIMAL_VIEWPORTS,
+      ...customViewports
+    }
   }
 }
 
