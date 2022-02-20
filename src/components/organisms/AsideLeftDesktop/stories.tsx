a4315315ad19e2401bd1bcd28c06dd2e1cc248b0
Maskalenkas <maskalenkasdoc@gmail.com>
2022-02-20 01:54:02 -0300

    asideLeftDesktop finalizado

---
+++ src/components/organisms/AsideLeftDesktop/stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react'
+import React from 'react'
+import styled from 'styled-components'
+
+import AsideLeftDesktop from '.'
+
+export default {
+  title: 'menudesktop/AsideLeftDesktop',
+  component: AsideLeftDesktop
+} as Meta
+
+export const Desktop: Story = (args) => (
+  <div style={{ width: '32rem' }}>
+    <AsideLeftDesktop {...args} />
+  </div>
+)
