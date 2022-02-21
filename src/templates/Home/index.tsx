de62d72e86b1b7ab7092f177c58437c8b324fc8e
Arthur <maskalenkasdoc@gmail.com>
2022-02-21 01:41:30 -0300

    fix

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,11 +1,19 @@
 import * as S from './styles'
 import React from 'react'
+import Layout from 'components/organisms/Layout'
+import Post from 'components/molecules/Post'
 
-import { IHomeProps } from './home'
-
-const Home = ({ default }: IHomeProps) => (
-  <S.HomeWrapper data-component-home>
-    <h1>home</h1>
+const Home = () => (
+  <S.HomeWrapper>
+    <Layout>
+      <Post
+        tag={{ title: 'misc', colorText: 'white', colorBackground: 'green' }}
+        date={{ day: 15, month: 5, year: 2000 }}
+        title="Meu ano de 2021"
+        shortText="Ano passado ja foi complicado para mim, mas esse ano foi mais ainda."
+        href="/"
+      />
+    </Layout>
   </S.HomeWrapper>
 )
 
