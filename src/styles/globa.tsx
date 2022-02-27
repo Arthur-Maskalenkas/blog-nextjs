e724b6ec3d82db9c44c022e31647cc49a3896fb3
Arthur <maskalenkasdoc@gmail.com>
2022-02-27 01:02:24 -0300

    estilo fixado

--- src/styles/globa.tsx
+++ src/styles/globa.tsx
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components'
 export const textWithHighlightGlobalStyle = styled.p`
   ${({ theme }) => css`
     color: ${theme.colors.texts};
+    cursor: pointer;
 
     &:hover {
       color: ${theme.colors.highlight};
