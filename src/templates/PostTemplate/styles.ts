16a80c580640a017e1348e26ca012eb9fcc2d79b
Arthur <maskalenkasdoc@gmail.com>
2022-02-28 03:30:36 -0300

    tema adicionado

--- src/templates/PostTemplate/styles.ts
+++ src/templates/PostTemplate/styles.ts
@@ -28,161 +28,140 @@ export const PostTemplateWrapper = styled.div`
 *
 */
 
-code[class*="language-"],
-pre[class*="language-"] {
-      color: #d6deeb;
-      font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
+
+    /**
+*
+* * --- --- --- --- Theme --- --- --- --- *
+*
+*/
+pre[class*=language-].line-numbers {
+      position: relative;
+      padding-left: 3.8em;
+      counter-reset: linenumber;
+    }
+    pre[class*='language-'].line-numbers > code {
+      position: relative;
+      white-space: inherit;
+    }
+    .line-numbers .line-numbers-rows {
+      position: absolute;
+      pointer-events: none;
+      top: 0;
+      font-size: 100%;
+      left: -3.8em;
+      width: 3em;
+      letter-spacing: -1px;
+      border-right: 1px solid #999;
+      -webkit-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+    .line-numbers-rows > span {
+      display: block;
+      counter-increment: linenumber;
+    }
+    .line-numbers-rows > span:before {
+      content: counter(linenumber);
+      color: #999;
+      display: block;
+      padding-right: 0.8em;
+      text-align: right;
+    }
+    code[class*='language-'],
+    pre[class*='language-'] {
+      color: #ccc;
+      background: none;
+      font-family: 'consolas'
+      font-size: 1.6rem;
       text-align: left;
       white-space: pre;
       word-spacing: normal;
       word-break: normal;
       word-wrap: normal;
       line-height: 1.5;
-      font-size: 1em;
-
       -moz-tab-size: 4;
-      -o-tab-size: 4;
+      -webkit-tab-size: 4;
       tab-size: 4;
-
       -webkit-hyphens: none;
-      -moz-hyphens: none;
       -ms-hyphens: none;
       hyphens: none;
     }
-
-    pre[class*='language-']::-moz-selection,
-    pre[class*='language-'] ::-moz-selection,
-    code[class*='language-']::-moz-selection,
-    code[class*='language-'] ::-moz-selection {
-      text-shadow: none;
-      background: rgba(29, 59, 83, 0.99);
-    }
-
-    pre[class*='language-']::selection,
-    pre[class*='language-'] ::selection,
-    code[class*='language-']::selection,
-    code[class*='language-'] ::selection {
-      text-shadow: none;
-      background: rgba(29, 59, 83, 0.99);
-    }
-
-    @media print {
-      code[class*='language-'],
-      pre[class*='language-'] {
-        text-shadow: none;
-      }
-    }
-
-    /* Code blocks */
     pre[class*='language-'] {
       padding: 1em;
       margin: 0.5em 0;
       overflow: auto;
     }
-
     :not(pre) > code[class*='language-'],
     pre[class*='language-'] {
-      color: white;
-      background: #011627;
+      background: #2d2d2d;
     }
-
     :not(pre) > code[class*='language-'] {
       padding: 0.1em;
       border-radius: 0.3em;
       white-space: normal;
     }
-
+    .token.block-comment,
+    .token.cdata,
     .token.comment,
-    .token.prolog,
-    .token.cdata {
-      color: rgb(99, 119, 119);
-      font-style: italic;
+    .token.doctype,
+    .token.prolog {
+      color: #999;
     }
-
     .token.punctuation {
-      color: rgb(199, 146, 234);
+      color: #ccc;
     }
-
-    .namespace {
-      color: rgb(178, 204, 214);
-    }
-
-    .token.deleted {
-      color: rgba(239, 83, 80, 0.56);
-      font-style: italic;
-    }
-
-    .token.symbol,
-    .token.property {
-      color: rgb(128, 203, 196);
-    }
-
-    .token.tag,
-    .token.operator,
-    .token.keyword {
-      color: rgb(127, 219, 202);
-    }
-
-    .token.boolean {
-      color: rgb(255, 88, 116);
-    }
-
-    .token.number {
-      color: rgb(247, 140, 108);
-    }
-
-    .token.constant,
-    .token.function,
-    .token.builtin,
-    .token.char {
-      color: rgb(130, 170, 255);
-    }
-
-    .token.selector,
-    .token.doctype {
-      color: rgb(199, 146, 234);
-      font-style: italic;
-    }
-
     .token.attr-name,
-    .token.inserted {
-      color: rgb(173, 219, 103);
-      font-style: italic;
+    .token.deleted,
+    .token.namespace,
+    .token.tag {
+      color: #e2777a;
     }
-
-    .token.string,
-    .token.url,
-    .token.entity,
-    .language-css .token.string,
-    .style .token.string {
-      color: rgb(173, 219, 103);
+    .token.function-name {
+      color: #6196cc;
     }
-
+    .token.boolean,
+    .token.function,
+    .token.number {
+      color: #f08d49;
+    }
     .token.class-name,
+    .token.constant,
+    .token.property,
+    .token.symbol {
+      color: #f8c555;
+    }
     .token.atrule,
-    .token.attr-value {
-      color: rgb(255, 203, 139);
+    .token.builtin,
+    .token.important,
+    .token.keyword,
+    .token.selector {
+      color: #cc99cd;
     }
-
+    .token.attr-value,
+    .token.char,
     .token.regex,
-    .token.important,
+    .token.string,
     .token.variable {
-      color: rgb(214, 222, 235);
+      color: #7ec699;
     }
-
-    .token.important,
-    .token.bold {
-      font-weight: bold;
+    .token.entity,
+    .token.operator,
+    .token.url {
+      color: #67cdcc;
     }
-
+    .token.bold,
+    .token.important {
+      font-weight: 700;
+    }
     .token.italic {
       font-style: italic;
     }
-    /**
-*
-* * --- --- --- --- Theme --- --- --- --- *
-*
-*/
+    .token.entity {
+      cursor: help;
+    }
+    .token.inserted {
+      color: green;
+    }
 
     /**
  *
