9571604e302ac06e2db20e4228c19869c980aacc
arthur <maskalenkasdoc@gmail.com>
2022-03-05 17:28:19 -0300

    icone alterado ao trocar de tema

--- src/components/molecules/ButtonChangeTheme/index.tsx
+++ src/components/molecules/ButtonChangeTheme/index.tsx
@@ -1,19 +1,25 @@
 import * as S from './styles'
 import React from 'react'
 import Icons from 'components/atoms/Icons'
-import theme from 'styles/theme'
 import { useChangeTheme } from 'hooks/useChangeTheme'
 
 const ButtonChangeTheme = () => {
-  const { toggleTheme } = useChangeTheme()
+  const { toggleTheme, currentThemeIsDark } = useChangeTheme()
+
+  const currentIcon = currentThemeIsDark ? 'lightOff' : 'lightOn'
+
+  const ariaLabelResolved = currentThemeIsDark
+    ? { 'aria-label': 'Alterar o tema da pagina para claro' }
+    : { 'aria-label': 'Alterar o tema da pagina para escuro' }
+
   return (
     <S.ButtonChangeThemeWrapper
       data-component-button-change-theme
-      aria-label="Alterar o tema da pagina"
+      {...ariaLabelResolved}
       onClick={() => toggleTheme()}
     >
       <Icons
-        icon="changeTheme"
+        icon={currentIcon}
         defaultStyle
         iconStyles={{
           size: '2.24',
--- src/components/molecules/ButtonChangeTheme/styles.ts
+++ src/components/molecules/ButtonChangeTheme/styles.ts
@@ -1,4 +1,8 @@
 import { Button } from 'components/atoms/Button/styles'
 import styled from 'styled-components'
 
-export const ButtonChangeThemeWrapper = styled(Button)``
+export const ButtonChangeThemeWrapper = styled(Button)`
+  &[aria-label='Alterar o tema da pagina para escuro'] svg {
+    color: #ffab00;
+  }
+`
