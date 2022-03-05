66b1d3a401ced72a518973a9ffd69821b70410fe
arthur <maskalenkasdoc@gmail.com>
2022-03-05 16:30:11 -0300

    nomenclatura dos icones trocadas

--- src/components/atoms/Icons/iconsCollection.tsx
+++ src/components/atoms/Icons/iconsCollection.tsx
@@ -2,8 +2,9 @@ import { Home } from '@styled-icons/boxicons-solid/Home'
 import { Search } from '@styled-icons/boxicons-regular/Search'
 import { Menu } from '@styled-icons/heroicons-outline/Menu'
 import { LightBulb } from '@styled-icons/entypo/LightBulb'
-import { ArrowUpShort } from 'styled-icons/bootstrap'
+import { ArrowUpShort, Lightbulb } from 'styled-icons/bootstrap'
 import { Github } from '@styled-icons/boxicons-logos/Github'
+import { LightbulbOff } from '@styled-icons/bootstrap/LightbulbOff'
 import { Linkedin } from '@styled-icons/boxicons-logos/Linkedin'
 
 import { ReactNode } from 'react'
@@ -11,7 +12,8 @@ import { ReactNode } from 'react'
 export interface IIconsList {
   home: ReactNode
   search: ReactNode
-  changeTheme: ReactNode
+  lightOff: ReactNode
+  lightOn: ReactNode
   goToTopPage: ReactNode
   github: ReactNode
   linkedin: ReactNode
@@ -19,7 +21,8 @@ export interface IIconsList {
 }
 
 export const IconsCollection: IIconsList = {
-  changeTheme: <LightBulb />,
+  lightOn: <LightBulb />,
+  lightOff: <LightbulbOff />,
   goToTopPage: <ArrowUpShort />,
   home: <Home />,
   search: <Search />,
