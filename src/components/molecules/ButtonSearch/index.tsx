ddcb857ec0e117057ee65f0ddead24357f989f6d
Maskalenkas <maskalenkasdoc@gmail.com>
2022-03-13 21:41:24 -0300

    função de redirecionar para a pagian de searchg adicionada

--- src/components/molecules/ButtonSearch/index.tsx
+++ src/components/molecules/ButtonSearch/index.tsx
@@ -1,21 +1,22 @@
 import * as S from './styles'
 import React from 'react'
 import Icons from 'components/atoms/Icons'
-import theme from 'styles/theme'
+import Link from 'next/link'
 
 const ButtonSearch = () => (
-  <S.ButtonSearchWrapper
-    data-component-button-search
-    aria-label="Pesquisar postagem no blog"
-  >
-    <Icons
-      icon="search"
-      iconStyles={{
-        size: '2.24',
-        sizeInDesktop: '2.5'
-      }}
-    />
-  </S.ButtonSearchWrapper>
+  <Link href={`/search`}>
+    <a aria-label="Pesquisar postagem no blog">
+      <S.ButtonSearchWrapper data-component-button-search>
+        <Icons
+          icon="search"
+          iconStyles={{
+            size: '2.24',
+            sizeInDesktop: '2.5'
+          }}
+        />
+      </S.ButtonSearchWrapper>
+    </a>
+  </Link>
 )
 
 export default ButtonSearch
