372a875d6d1928b10b18f792d2850625ffa7f1e0
Maskalenkas <maskalenkasdoc@gmail.com>
2022-04-15 14:20:44 -0300

    feat: button to top add

--- src/components/molecules/ButtonGoToTopPage/index.tsx
+++ src/components/molecules/ButtonGoToTopPage/index.tsx
@@ -3,19 +3,28 @@ import React from 'react'
 import Icons from 'components/atoms/Icons'
 import theme from 'styles/theme'
 
-const ButtonGoToTopPage = () => (
-  <S.ButtonGoToTopPageWrapper
-    data-component-button-go-to-top-page
-    aria-label="ir para o topo da pagina"
-  >
-    <Icons
-      icon="goToTopPage"
-      iconStyles={{
-        size: '3.34',
-        sizeInDesktop: '2.5'
-      }}
-    />
-  </S.ButtonGoToTopPageWrapper>
-)
+const ButtonGoToTopPage = () => {
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
 
+  return (
+    <S.ButtonGoToTopPageWrapper
+      data-component-button-go-to-top-page
+      aria-label="ir para o topo da pagina"
+      onClick={() => handleClick()}
+    >
+      <Icons
+        icon="goToTopPage"
+        iconStyles={{
+          size: '3.34',
+          sizeInDesktop: '2.5'
+        }}
+      />
+    </S.ButtonGoToTopPageWrapper>
+  )
+}
 export default ButtonGoToTopPage
