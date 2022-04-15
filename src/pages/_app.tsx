03bc356349201b59beac457769d83a10d7b00474
Maskalenkas <maskalenkasdoc@gmail.com>
2022-04-15 18:58:13 -0300

    refactor: antigo theme provider removido

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,51 +10,30 @@ import GlobalStyles from 'styles/global'
 
 import { MenuMobileProvider } from 'hooks/useMenuMobile'
 import theme, { themeColors } from 'styles/theme'
-import { useEffect, useState } from 'react'
-import { ChangeThemeProvider } from 'hooks/useChangeTheme'
-import { getStorageItem } from 'helpers/localStorage'
 import NextNProgress from 'nextjs-progressbar'
 
 function App({ Component, pageProps }: AppProps) {
-  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false)
-
-  const colorsInThemeResolved = {
-    ...theme,
-    colors: isDarkTheme ? themeColors.dark : themeColors.light
-  }
-
-  useEffect(() => {
-    setIsDarkTheme(getStorageItem('theme')[0] == 'dark')
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [getStorageItem('theme')])
-
   return (
     <>
-      <ChangeThemeProvider
-        currentThemeIsDark={isDarkTheme}
-        toggleTheme={setIsDarkTheme}
-      >
-        {/* @ts-nocehck */}
-        <ThemeProvider theme={colorsInThemeResolved}>
-          <MenuMobileProvider>
-            <Head>
-              <title>Arthur Maskalenkas</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-            </Head>
-            <GlobalStyles />
-            <Component {...pageProps} />
-            <NextNProgress
-              color="#F231A5"
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={5}
-              showSpinner={false}
-            />
-          </MenuMobileProvider>
-        </ThemeProvider>
-      </ChangeThemeProvider>
+      <ThemeProvider theme={theme}>
+        <MenuMobileProvider>
+          <Head>
+            <title>Arthur Maskalenkas</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+          <NextNProgress
+            color="#F231A5"
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={5}
+            showSpinner={false}
+          />
+        </MenuMobileProvider>
+      </ThemeProvider>
     </>
   )
 }
