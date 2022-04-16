6a0d58b0e4264521340ee28ac1c3876fab5bbf4b
Maskalenkas <maskalenkasdoc@gmail.com>
2022-04-15 23:39:50 -0300

    chore: lib para analisar o bundle adicionado

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
-module.exports = withPWA({
+module.exports = withBundleAnalyzer({
   swcMinify: true,
   experimental: {
     // Enables the styled-components SWC transform
     styledComponents: true
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProd
   }
 })
