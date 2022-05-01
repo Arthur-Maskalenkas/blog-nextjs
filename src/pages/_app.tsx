77b9d4894e257942db310b5794faeccf49689a7e
Maskalenkas <maskalenkasdoc@gmail.com>
2022-04-30 22:05:08 -0300

    Analytics adicionado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,25 @@ import { MenuMobileProvider } from 'hooks/useMenuMobile'
 import theme from 'styles/theme'
 import NextNProgress from 'nextjs-progressbar'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as gtag from 'helpers/Gtag'
+import Analytics from 'helpers/Analytics'
+
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -40,6 +58,7 @@ function App({ Component, pageProps }: AppProps) {
             height={5}
             showSpinner={false}
           />
+          <Analytics />
         </MenuMobileProvider>
       </ThemeProvider>
     </>
