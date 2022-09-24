755ccdd4a6c5381fd02d660386a4e1b52fcc2e5c
Maskalenkas <maskalenkasdoc@gmail.com>
2022-09-23 23:14:24 -0300

    fix

--- src/components/molecules/Search/index.tsx
+++ src/components/molecules/Search/index.tsx
@@ -11,14 +11,13 @@ import Icons from 'components/atoms/Icons'
 
 const Search = () => {
   const searchClient = algoliasearch(
-    process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || '',
-    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || ''
+
   )
 
   return (
     <S.SearchWrapper data-component-search>
       <InstantSearch
-        indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME || ''}
+        indexName={'blog_nextjs'}
         searchClient={searchClient}
       >
         <div>
